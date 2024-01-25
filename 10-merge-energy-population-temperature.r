14de8aeae2f5029976e0dfe44858b8c7286bf9ba
stevayob <david.stegmaier@ut-capitole.fr>
2024-01-25 16:12:28 +0100

    First try to merge

---
+++ 10-merge-energy-population-temperature.r
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(arrow)
+
+
+
+data_dir <- 'C:/Users/David/Documents/VWL/Master Toulouse/Semester 2 M1/Applied  Metrics Project/Data'
+
+file_path_parquet <- file.path(data_dir, '04-joined.parquet')
+file_path_csv <- file.path(data_dir,'09-temp-pop-merged.csv' )
+
+energy <- read_parquet(file_path_parquet)
+temp_pop <- read.csv(file_path_csv)
+
+
+# Assuming 'date' is your date column
+temp_pop$Date <- as.POSIXct(temp_pop$Date, format="%Y-%m-%d")
+temp_pop$Date <- as.POSIXct(paste(temp_pop$Date, "00:00:00"), format="%Y-%m-%d %H:%M:%S")
+
+# Merge data frames
+temp_pop <- temp_pop %>%  rename("interval_end" = "Date")
+energy_n <- left_join(energy, temp_pop, by = c("interval_end", "regionid"))
+energy_n <- energy_n %>%  fill(Temperature, .direction = "down")
+view(energy_n)
