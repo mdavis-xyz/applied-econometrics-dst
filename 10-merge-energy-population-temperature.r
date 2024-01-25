f6b210b5c1aaeb2ce5016fae3a6e45e6ad20d437
stevayob <david.stegmaier@ut-capitole.fr>
2024-01-25 17:43:12 +0100

    Fix data directory path

--- 10-merge-energy-population-temperature.r
+++ 10-merge-energy-population-temperature.r
@@ -3,8 +3,7 @@ library(arrow)
 
 
 
-data_dir <- "C:/Users/David/Documents/VWL/Master Toulouse/
-                Semester 2 M1/Applied  Metrics Project/Data"
+data_dir <- "C:/Users/David/Documents/VWL/Master Toulouse/Semester 2 M1/Applied  Metrics Project/Data"
 
 file_path_parquet <- file.path(data_dir, "04-joined.parquet")
 file_path_csv <- file.path(data_dir, "09-temp-pop-merged.csv")
@@ -15,19 +14,14 @@ temp_pop <- read.csv(file_path_csv)
 
 # Add time to temp_pop
 temp_pop <- temp_pop %>%  rename("interval_end" = "Date")
-temp_pop$Date <- as.POSIXct(temp_pop$Date, format = "%Y-%m-%d")
-temp_pop$Date <- as.POSIXct(paste(temp_pop$Date, "00:00:00"),
-                            format = "%Y-%m-%d %H:%M:%S")
+temp_pop$interval_end <- as.POSIXct(temp_pop$interval_end, format = "%Y-%m-%d")
+temp_pop$interval_end <- as.POSIXct(paste(temp_pop$interval_end, "00:00:00"),
+                                    format = "%Y-%m-%d %H:%M:%S")
 
 
-temp_pop$interval_end <- as.POSIXct(temp_pop$interval_end, format = "%Y-%m-%d %H:%M:%S")
-
-
-class(temp_pop$interval_end)
-class(energy$interval_end)
-
-
+#Merge
 energy_n <- left_join(energy, temp_pop, by = c("interval_end", "regionid"))
 energy_n <- energy_n %>%  fill(Temperature, .direction = "down") %>% fill(Population, .direction = "down")
 
-view(energy_n)
+#Save
+write.csv(energy_n, file = file.path(data_dir, "10-energy-merged.csv"))
