2de5d5a956f2eb5999aa438d27cbc1595e837a9d
Matthew Davis <github@mdavis.xyz>
2024-02-17 18:26:52 +0100

    add prior trend graph

--- 14-common-trend-graphs.R
+++ 14-common-trend-graphs.R
@@ -9,12 +9,12 @@ daily = read_parquet(file.path(data_dir, "12-energy-daily.parquet"))
 
 hourly <- hourly |> 
   mutate(
-    co2_ratio = co2_per_capita / co2_midday_per_capita
+    co2_kg_ratio = co2_kg_per_capita / co2_kg_midday_per_capita
   )
 
 daily <- daily |> 
   mutate(
-    co2_ratio = co2_per_capita / co2_midday_per_capita
+    co2_kg_ratio = co2_kg_per_capita / co2_kg_midday_per_capita
   )
 
 # normalise emissions to 100% on the day of transition, per region
@@ -25,20 +25,20 @@ daily <- daily |>
   select(
     regionid,
     dst_date,
-    energy_mwh_per_capita,
-    co2_per_capita,
-    co2_ratio,
+    energy_kwh_per_capita,
+    co2_kg_per_capita,
+    co2_kg_ratio,
   ) |>
   rename(
-    energy_mwh_per_capita_at_transition=energy_mwh_per_capita,
-    co2_per_capita_at_transition=co2_per_capita,
-    co2_ratio_at_transition=co2_ratio,
+    energy_kwh_per_capita_at_transition=energy_kwh_per_capita,
+    co2_kg_per_capita_at_transition=co2_kg_per_capita,
+    co2_kg_ratio_at_transition=co2_kg_ratio,
   ) |>
   right_join(daily) |>
   mutate(
-    energy_mwh_per_capita_compared_to_transition=energy_mwh_per_capita/energy_mwh_per_capita_at_transition,
-    co2_per_capita_compared_to_transition=co2_per_capita / co2_per_capita_at_transition,
-    co2_ratio_compared_to_transition=co2_ratio / co2_ratio_at_transition
+    energy_kwh_per_capita_compared_to_transition=energy_kwh_per_capita/energy_kwh_per_capita_at_transition,
+    co2_kg_per_capita_compared_to_transition=co2_kg_per_capita / co2_kg_per_capita_at_transition,
+    co2_kg_ratio_compared_to_transition=co2_kg_ratio / co2_kg_ratio_at_transition
   )
 hourly <- hourly |>
   filter(
@@ -48,33 +48,34 @@ hourly <- hourly |>
   select(
     regionid,
     dst_date,
-    energy_mwh_per_capita,
-    co2_per_capita,
-    co2_ratio,
+    energy_kwh_per_capita,
+    co2_kg_per_capita,
+    co2_kg_ratio,
   ) |>
   rename(
-    energy_mwh_per_capita_at_transition=energy_mwh_per_capita,
-    co2_per_capita_at_transition=co2_per_capita,
-    co2_ratio_at_transition=co2_ratio,
+    energy_kwh_per_capita_at_transition=energy_kwh_per_capita,
+    co2_kg_per_capita_at_transition=co2_kg_per_capita,
+    co2_kg_ratio_at_transition=co2_kg_ratio,
   ) |>
   right_join(hourly) |>
   mutate(
-    energy_mwh_per_capita_compared_to_transition=energy_mwh_per_capita/energy_mwh_per_capita_at_transition,
-    co2_per_capita_compared_to_transition=co2_per_capita / co2_per_capita_at_transition,
-    co2_ratio_compared_to_transition=co2_ratio / co2_ratio_at_transition
+    energy_kwh_per_capita_compared_to_transition=energy_kwh_per_capita/energy_kwh_per_capita_at_transition,
+    co2_kg_per_capita_compared_to_transition=co2_kg_per_capita / co2_kg_per_capita_at_transition,
+    co2_kg_ratio_compared_to_transition=co2_kg_ratio / co2_kg_ratio_at_transition
   )
 
 
 hourly |>
-  #filter(regionid != 'TAS1') |>
-  #filter(regionid != 'SA1') |>
-  ggplot(aes(x=days_into_dst, y=co2_per_capita_compared_to_transition, color=regionid)) +
-  geom_smooth()
+  filter(regionid != 'TAS1') |>
+  filter(regionid != 'SA1') |>
+  ggplot(aes(x=days_into_dst, y=co2_kg_per_capita, color=regionid)) +
+  geom_smooth() +
+  geom_point(alpha=0.001)
 
 hourly |> 
   filter(regionid == 'VIC1') |>
   filter(Date == first(Date)) |>
-  ggplot(aes(x=hr, y=co2)) +
+  ggplot(aes(x=hr, y=co2_kg_per_capita)) +
   geom_smooth()
 
 # aggregate all treatment states together
@@ -83,12 +84,12 @@ agg_d <- daily |>
     treatment = (regionid != 'QLD1')
   ) |>
   summarise(
-    co2=sum(co2, na.rm=TRUE),
+    co2_kg=sum(co2_kg, na.rm=TRUE),
     population=sum(population, na.rm=TRUE),
     .by=c(treatment, Date, days_into_dst, dst_date, dst_transition_id)
   ) |>
   mutate(
-    co2_per_capita = co2 / population
+    co2_kg_per_capita = co2_kg / population
   )
 
 
@@ -100,24 +101,24 @@ agg_d <- agg_d |>
   select(
     treatment,
     dst_date,
-    co2_per_capita,
+    co2_kg_per_capita,
   ) |>
   rename(
-    co2_per_capita_at_transition=co2_per_capita,
+    co2_kg_per_capita_at_transition=co2_kg_per_capita,
   ) |>
   right_join(agg_d) |>
   mutate(
-    co2_per_capita_compared_to_transition=co2_per_capita / co2_per_capita_at_transition,
+    co2_kg_per_capita_compared_to_transition=co2_kg_per_capita / co2_kg_per_capita_at_transition,
   )
 
 agg_d |>
-  ggplot(aes(x=days_into_dst, co2_per_capita_compared_to_transition, color=treatment)) + 
+  ggplot(aes(x=days_into_dst, co2_kg_per_capita_compared_to_transition, color=treatment)) + 
   geom_smooth() + 
   labs(
     title="Event Study - DST vs Emissions - grouped",
     subtitle="Common trend present when not in DST",
     x = "Days after DST start/before DST end",
-    y = "CO2 Per capita, normalised",
+    y = "co2_kg Per capita, normalised",
   )
 ggsave("plots/event-study-all-treatment.png", height=7, width=9)
 
@@ -128,12 +129,12 @@ agg_d2 <- daily |>
     treatment = (regionid != 'QLD1')
   ) |>
   summarise(
-    co2=sum(co2, na.rm=TRUE),
+    co2_kg=sum(co2_kg, na.rm=TRUE),
     population=sum(population, na.rm=TRUE),
     .by=c(treatment, Date, days_into_dst, dst_date, dst_direction)
   ) |>
   mutate(
-    co2_per_capita = co2 / population
+    co2_kg_per_capita = co2_kg / population
   )
 
 
@@ -145,23 +146,23 @@ agg_d2 <- agg_d2 |>
   select(
     treatment,
     dst_date,
-    co2_per_capita,
+    co2_kg_per_capita,
   ) |>
   rename(
-    co2_per_capita_at_transition=co2_per_capita,
+    co2_kg_per_capita_at_transition=co2_kg_per_capita,
   ) |>
   right_join(agg_d2) |>
   mutate(
-    co2_per_capita_compared_to_transition=co2_per_capita / co2_per_capita_at_transition,
+    co2_kg_per_capita_compared_to_transition=co2_kg_per_capita / co2_kg_per_capita_at_transition,
   )
 
 agg_d2 |>
   filter(dst_direction == 'start') |>
-  ggplot(aes(x=days_into_dst, co2_per_capita_compared_to_transition, color=paste(treatment, dst_direction))) + 
+  ggplot(aes(x=days_into_dst, co2_kg_per_capita_compared_to_transition, color=paste(treatment, dst_direction))) + 
   geom_smooth()
 
 
 agg_d2 |>
   filter(dst_direction == 'stop') |>
-  ggplot(aes(x=days_into_dst, co2_per_capita_compared_to_transition, color=paste(treatment, dst_direction))) + 
+  ggplot(aes(x=days_into_dst, co2_kg_per_capita_compared_to_transition, color=paste(treatment, dst_direction))) + 
   geom_smooth()
