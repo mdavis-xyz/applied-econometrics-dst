7441af4f329b832d3876a583947d2c59461822db
Matthew Davis <github@mdavis.xyz>
2024-02-25 22:06:31 +0100

    make coding style uniform

--- 07-weather-data-cleaner.R
+++ 07-weather-data-cleaner.R
@@ -26,13 +26,13 @@ kwh_per_megajoule = 10^3 / (60^2)
 # This is for temperature, which drives load, which is mostly in cities.
 # the second one is for wind and solar, in the middle of the regions.
 # This drives generation, which is dispersed across the region.
-city_region_map_1 <- c(
+capital_city_region_map <- c(
   'adelaide' = 'SA1', 
   'brisbane' = 'QLD1',
   'sydney' = 'NSW1',
   'melbourne' = 'VIC1',
   'hobart' = 'TAS1')
-city_region_map_2 <- c(
+regional_city_region_map <- c(
   'cooberpedy' = 'SA1',
   'richmond' = 'QLD1',
   'dubbo' = 'NSW1',
@@ -47,27 +47,27 @@ clean_and_combine_temp <- function(file_path) {
   temperature_data <- read_csv(file_path)
   
   # Clean Data
-  temperature_data <- temperature_data %>%
-    mutate(Date = make_date(Year, Month, Day)) %>%
+  temperature_data <- temperature_data |>
+    mutate(Date = make_date(Year, Month, Day)) |>
     select(-c(`Product code`, 
               `Bureau of Meteorology station number`,
               `Days of accumulation of maximum temperature`,
               Quality,
               Year,
               Month, 
-              Day)) %>%
-    filter(Date >= as.Date(paste(first_year, "-01-01", sep = ""))) %>%
+              Day)) |>
+    filter(Date >= as.Date(paste(first_year, "-01-01", sep = ""))) |>
     rename(temperature = `Maximum temperature (Degree C)`)
   
   # Correct NaN
-  temperature_data <- temperature_data %>%
-    mutate(rolling_mean = rollapply(temperature, 3, mean, align = "center", fill = NA)) %>%
-    mutate(temperature = ifelse(is.na(temperature), rolling_mean, temperature)) %>%
+  temperature_data <- temperature_data |>
+    mutate(rolling_mean = rollapply(temperature, 3, mean, align = "center", fill = NA)) |>
+    mutate(temperature = ifelse(is.na(temperature), rolling_mean, temperature)) |>
     select(-rolling_mean)
   
   # Extract the city name from the file name
   city_name <- str_remove(str_remove(basename(file_path), 'weather_'), '.csv')
-  region_code <- city_region_map_1[city_name]
+  region_code <- capital_city_region_map[city_name]
   temperature_data$regionid <- region_code
   
   # Return cleaned data
@@ -92,26 +92,26 @@ clean_and_combine_sunshine <- function(file_path) {
   sunshine_data <- read_csv(file_path)
   
   # Clean Data
-  sunshine_data <- sunshine_data %>%
-    mutate(Date = make_date(Year, Month, Day)) %>%
+  sunshine_data <- sunshine_data |>
+    mutate(Date = make_date(Year, Month, Day)) |>
     select(-c(`Product code`,
               `Bureau of Meteorology station number`,
               Year, 
               Month, 
-              Day)) %>%
-    rename(solar_exposure = `Daily global solar exposure (MJ/m*m)`) %>%
-    mutate(solar_exposure = solar_exposure * kwh_per_megajoule)  %>%
+              Day)) |>
+    rename(solar_exposure = `Daily global solar exposure (MJ/m*m)`) |>
+    mutate(solar_exposure = solar_exposure * kwh_per_megajoule)  |>
     filter(Date >= as.Date(paste(first_year, "-01-01", sep = "")))
   
   # Correct NaN
-  sunshine_data <- sunshine_data %>%
-    mutate(rolling_mean = rollapply(solar_exposure, 3, mean, align = "center", fill = NA)) %>%
-    mutate(solar_exposure = ifelse(is.na(solar_exposure), rolling_mean, solar_exposure)) %>%
+  sunshine_data <- sunshine_data |>
+    mutate(rolling_mean = rollapply(solar_exposure, 3, mean, align = "center", fill = NA)) |>
+    mutate(solar_exposure = ifelse(is.na(solar_exposure), rolling_mean, solar_exposure)) |>
     select(-rolling_mean)
   
   # Extract the city name from the file name
   city_name <- str_remove(str_remove(basename(file_path), 'sunshine-'), '.csv')
-  region_code <- city_region_map_2[city_name]
+  region_code <- regional_city_region_map[city_name]
   print(paste("Trying to find region for city", city_name, ", found ", region_code))
   stopifnot(! is.na(region_code))
   sunshine_data$regionid <- region_code
@@ -136,9 +136,9 @@ solar <- bind_rows(all_sunshine)
 merged_weather <- left_join(temperature, solar, by= c("Date", "regionid"))
 
 # Fill in gaps which are larger than one day in a row by interpolating linearly 
-merged_weather <- merged_weather %>%
-  group_by(regionid) %>%
-  mutate(solar_exposure = approx(x = 1:n(), y = solar_exposure, method = "linear", n = n())$y) %>% 
+merged_weather <- merged_weather |>
+  group_by(regionid) |>
+  mutate(solar_exposure = approx(x = 1:n(), y = solar_exposure, method = "linear", n = n())$y) |> 
   mutate(temperature = approx(x = 1:n(), y = temperature, method = "linear", n = n())$y)
 
 # Save merged data to CSV
--- 09-population-weather-merger.R
+++ 09-population-weather-merger.R
@@ -20,15 +20,15 @@ merged_df <- merge(population, weather, by = c("regionid", "Date"), all = TRUE)
 merged_df <- merged_df[order(merged_df$regionid, merged_df$Date), ]
 
 # Forward fill missing values in Population column within each group
-merged_df <- merged_df %>%
-  arrange(regionid, Date) %>% 
-  group_by(regionid) %>% 
+merged_df <- merged_df |>
+  arrange(regionid, Date) |> 
+  group_by(regionid) |> 
   mutate(population = approx(x = 1:n(), y = population, method = "linear", n = n())$y)
 
 # Drop Dates before 2009 and after 2023
-merged_df <- merged_df %>% filter(!(Date <= as.Date("2008-12-31") | Date >= as.Date("2024-01-01")))
+merged_df <- merged_df |> filter(!(Date <= as.Date("2008-12-31") | Date >= as.Date("2024-01-01")))
 
 
 # Save merged data to CSV
 write_csv(merged_df, file.path(data_dir, '09-temp-pop-merged.csv'))
-cat('All data merged and saved to CSV')
+print('All data merged and saved to CSV')
