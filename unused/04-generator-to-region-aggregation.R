23912cca2f494e2a1f3239fa22fa9e30738d52ed
Matthew Davis <github@mdavis.xyz>
2024-02-28 22:46:47 +0100

    remove unused folder

--- unused/04-generator-to-region-aggregation.R
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-library(arrow)
-data_dir <- '/media/matthew/Tux/AppliedEconometrics/data'
-
-ds <- open_dataset('/media/matthew/Tux/AppliedEconometrics/data/02-C-deduplicated/DISPATCHLOAD')
-ds |> collect()
